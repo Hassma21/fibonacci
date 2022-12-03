1174a5129aad9a310c6003531636aca44396d592
muhammed yılmaz <muhammedyilmazyilmaz@gmail.com>
2022-12-03 22:40:56 +0300

    Baslanggıc dosyaları gönderildi

---
+++ app/src/main/java/com/muhammed/fibonacci/app.kt
@@ -0,0 +1,19 @@
+package com.muhammed.fibonacci
+
+fun main(){
+    fibonacci(10)
+}
+fun fibonacci(count:Int){
+
+    var firstValue = 0
+    var secondValue = 1
+    var arrayFibonacci= arrayListOf<Int>()
+    arrayFibonacci.add(firstValue)
+    arrayFibonacci.add(secondValue)
+
+    for (i in 0..count-3) {
+        val sum = arrayFibonacci[i] + arrayFibonacci[i+1]
+        arrayFibonacci.add(sum)
+    }
+    println(arrayFibonacci)
+}
